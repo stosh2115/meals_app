2208b0106fd0ac11eccf3ab69c7058a771cc0d4a
stosh2115 <stosh09@hotmail.com>
2024-04-23 21:10:48 -0700

    MealDetails

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import CategoriesScreen from './screens/CategoriesScreen';
 import MealsOverviewScreen from './screens/MealsOverviewScreen';
+import MealDetailScreen from './screens/MealDetailScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -40,6 +41,7 @@ export default function App() {
 
             // }}
           />
+          <Stack.Screen name='MealDetail' component={MealDetailScreen}/> 
         </Stack.Navigator>
       </NavigationContainer>
 
--- components/MealItem.js
+++ components/MealItem.js
@@ -1,25 +1,34 @@
 import { View, Pressable, Text, Image, StyleSheet, Platform } from 'react-native'
 
-function MealItem({title, imageUrl, duration, complexity, affordability}) {
-    return(
+import { useNavigation } from '@react-navigation/native';
+import MealDetails from './MealDetails';
+
+
+function MealItem({id, title, imageUrl, duration, complexity, affordability}) {
+    const navigation = useNavigation();
+
+    function selectMeallItemHandler() {
+        navigation.navigate('MealDetail', {
+            mealId: id
+        });
+
+    }
+
+
+
+    return (
         <View style={styles.mealItem}>
             <Pressable 
                 android_ripple={{color: '#ccc'}}
-                style={({pressed}) => [
-                styles.button, pressed ? 
-                styles.buttonPressed : null,
-                ]}
+                style={({pressed}) =>  ( pressed ? styles.buttonPressed : null )}
+                onPress={selectMeallItemHandler}
             >
                 <View style={styles.innerContainer}>
                     <View>
                         <Image source={{ uri: imageUrl }} style={styles.image} />
                         <Text style={styles.title}>{title}</Text>
                     </View>
-                    <View style={styles.details}>
-                        <Text style={styles.detailItem}>{duration}m</Text>
-                        <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
-                        <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
-                    </View>
+                    <MealDetails duration={duration} affordability={affordability} complexity={complexity}/>
                 </View>
             </Pressable>
         </View>
